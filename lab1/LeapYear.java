b80ea25a17956b8e12468d0c4a4b27c41ee6d3b6
pkmnfreak <evcchang@berkeley.edu>
2017-01-18 13:58:52 -0800

    Finished lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,16 @@ public class LeapYear {
             }
         }
     }
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 }
-
