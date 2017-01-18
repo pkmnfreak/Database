5904855c1087bd061714ed1b76ff20febfa1c912
pkmnfreak <evcchang@berkeley.edu>
2017-01-18 14:02:59 -0800

    Finished lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -2,7 +2,7 @@ public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
         int Total = 0;
-        while (x < 11) {
+        while (x < 10) {
             System.out.print(Total + " ");
             x = x + 1;
             Total += x;
